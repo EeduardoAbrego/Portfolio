38e0770aeaf8f9498c692f7f42e834f9bcb419ef
eduardoabrego11 <eduardoabrego11@gmail.com>
2024-05-24 11:41:09 -0300

    migrando a Tailwind Head

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './styles/**/*.css'],
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx}', './styles/**/*.css'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-bg': '#fefeff10',
+        'custom-blue1': 'rgb(53, 96, 189) ',
+      },
+      colors: {
+        'custom-blue': 'rgb(53, 96, 189)',
+        'custom-blue-1': 'rgb(2, 28, 112)',
+      },
+      backdropFilter: {
+        'custom-blur': 'blur(0.2rem)',
+      },
+      dropShadow: {
+        '3xl': ' 3px 5px 0 rgb(10, 26, 172);',
+      },
+      keyframes: {
+        slidein: {
+          from: { 'margin-left': '100%', width: '300%' },
+          to: { 'margin-left': '0%', width: '100%' },
+        },
+      },
+      animation: {
+        slidein: ' slidein 2s ',
+      },
+    },
   },
   plugins: [],
-}
-
+};
